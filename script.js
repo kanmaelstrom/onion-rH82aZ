56c095c691730f6a6703e96dae0e86ce7bad54ca
Kan Maelstrom <kanmaelstrom@gmail.com>
2026-02-01 23:03:05 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("background");
+const ctx = canvas.getContext("2d");
+
+const cw = canvas.width;
+const ch = canvas.height;
+
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 20,20);
